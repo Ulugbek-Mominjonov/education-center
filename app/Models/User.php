04b0cc30f57c0a248d2d20a1d527faf3bb7f9586
ulugbek-mominjonov <ulugbeknabijonovich@gmail.com>
2025-01-22 14:40:51 +0500

    database migrated

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
 
     public function roles(): BelongsToMany
     {
-        return $this->belongsToMany(RolesUsers::class);
+        return $this->belongsToMany(Role::class);
     }
 
     public function teacher(): HasOne
